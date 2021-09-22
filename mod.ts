1b8f655bc6bab026ca8e093925c0d160e4e2fb1a
William Dussault <dalloriam@gmail.com>
2021-09-22 17:55:00 -0400

    Initial commit

---
+++ mod.ts
@@ -0,0 +1,48 @@
+import * as fs from 'http://deno.land/std@0.107.0/fs/mod.ts';
+import * as path from 'https://deno.land/std@0.107.0/path/mod.ts';
+
+function getBaseConfigDir(): string {
+    switch (Deno.build.os) {
+        case 'linux': {
+            const xdg = Deno.env.get('XDG_CONFIG_HOME');
+            if (xdg) {
+                return xdg;
+            }
+
+            const home = Deno.env.get('HOME');
+            if (home) {
+                return `${home}/.config`;
+            }
+            break;
+        }
+        case 'darwin': {
+            const home = Deno.env.get('HOME');
+            if (home) {
+                return `${home}/Library/Application Support`;
+            }
+            break;
+        }
+
+        case 'windows': {
+            const cfgDir = Deno.env.get('FOLDERID_LocalAppData');
+            if (cfgDir) {
+                return cfgDir;
+            }
+            break;
+        }
+    }
+
+    throw Error('didn\'t find configuration directory');
+}
+
+export async function loadConfig<T>(namespace: string, name: string) {
+    const configDir = getBaseConfigDir();
+    const configPath = path.join(configDir, namespace, name) + '.json';
+
+    if (!await fs.exists(configPath)) {
+        throw Error(`config file '${configPath} does not exist'`);
+    }
+
+    const rawConfig = await Deno.readTextFile(configPath);
+    return JSON.parse(rawConfig) as T;
+}
